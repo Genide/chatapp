560ed592df9b1b3dafabfa99ea90995aba91e707
Daniel Huan Nguyen <danielhuannguyen@gmail.com>
2015-11-26 15:09:41 -0600

    moved server into index.js"

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+server.listen(8080);
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	res.render('index', { title: 'Express' });
+});
+
+io.on('connection', function(socket) {
+	socket.broadcast.emit('chat message', 'A user has connected');
+
+	socket.on('chat message', function(msg){
+		io.emit('chat message', msg);
+	});
+
+	socket.on('disconnect', function() {
+		socket.broadcast.emit('chat message', 'A user has disconnected');
+	});
 });
 
 module.exports = router;
